0c1ed267704fb1afe64d2ff3d415dd5cdd21146d
Niklas Maderbacher <maderb190007@sr.htlweiz.at>
2024-09-24 22:53:48 +0200

    Renamed variables and added documentation. Also added functionality to create new node

--- src/read_file.c
+++ src/read_file.c
@@ -4,57 +4,77 @@ FILE *read_file(const char *filepath) { return fopen(filepath, "r"); }
 
 void close_file(const FILE *file) { fclose(file); }
 
-struct DBSingleLinkedList extract_documentation_py(const FILE *py_file) {
+struct DBSingleLinkedListNode extract_documentation_py(const FILE *py_file) {
   char buffer[256];
-  bool at_docs = false;
-  bool name_saved = false;
-  bool before_cols = false;
-  bool at_cols = false;
-  DBSingleLinkedList *entire_db;
-  DBTableSingleLinkedList *db_table;
-  DBColumnSingleLinkedList *db_column;
+  bool cur_line_at_docs = false;
+  bool table_name_saved = false;
+  bool cur_line_before_cols = false;
+  bool cur_line_at_cols = false;
+  DBSingleLinkedListNode *cur_db_data_node;
+  DBTableSingleLinkedListNode *cur_table_node;
+  DBColumnSingleLinkedListNode *cur_col_node;
 
   // set last element pointers to NULLPOINTER
-  entire_db->next_struct = NULL;
-  db_table->next_struct = NULL;
-  db_column->next_struct = NULL;
+  cur_db_data_node->previous_node = NULL;
+  cur_table_node->previous_node = NULL;
+  cur_col_node->previous_node = NULL;
 
   while (fgets(buffer, sizeof(buffer), py_file) != NULL) {
-    char old_buffer[256];
     // remove leading spaces for easier work
     RemoveLeadingSpaces(buffer);
 
     // end of documentation, reset all values
-    if ((strncmp(buffer, "__tablename__", 13) == 0) && at_docs && at_cols) {
-      at_docs = false;
-      at_cols = false;
-      name_saved = false;
+    if ((strncmp(buffer, "\"\"\"", 3) == 0) && cur_line_at_docs &&
+        cur_line_at_cols) {
+      cur_line_at_docs = false;
+      cur_line_at_cols = false;
+      table_name_saved = false;
 
-      // set final
-      entire_db->tables = db_table;
+      // set table node in db data node
+      cur_db_data_node->table_node = cur_table_node;
+
+      // create new node
+      // store current node in previous node variable of new node
+      // and set current node to new node
+      DBSingleLinkedListNode *new_db_data_node;
+      new_db_data_node->previous_node = cur_db_data_node;
+      cur_db_data_node = new_db_data_node;
     }
 
-    if (before_cols && at_docs) {
-      at_cols = true;
-      before_cols = false;
+    // skip one iteration of loop
+    // line between documentation start point and actual data is not needed
+    if (cur_line_before_cols && cur_line_at_docs) {
+      cur_line_at_cols = true;
+      cur_line_before_cols = false;
     }
 
-    if (at_docs && strncmp(buffer, "\"\"\"", 3) != 0 && !at_cols &&
-        !name_saved) {
-      db_table->table_name = StoreUntilSpace(buffer);
-      name_saved = true;
+    if (cur_line_at_docs && strncmp(buffer, "\"\"\"", 3) != 0 &&
+        !cur_line_at_cols && !table_name_saved) {
+      cur_table_node->table_name = StoreUntilSpace(buffer);
+      table_name_saved = true;
     }
 
-    else if (at_docs && at_cols) {
+    // done
+    else if (cur_line_at_docs && cur_line_at_cols) {
+      DBColumnSingleLinkedListNode *new_db_col_node;
+      cur_col_node->column_name = StoreUntilSpace(buffer);
+      cur_col_node->column_type = ExtractValueInBrackets(buffer);
+
+      // store old node in new nodes previous node variable
+      new_db_col_node->previous_node = cur_col_node;
+
+      // set current node to new and empty node with exception,
+      // that the previous node is stored in previous node variable
+      cur_col_node = new_db_col_node;
     }
 
     // check if docs begin or end
-    if (strncmp(buffer, "class", 5) == 0 && !at_docs) {
-      at_docs = true;
+    if (strncmp(buffer, "class", 5) == 0 && !cur_line_at_docs) {
+      cur_line_at_docs = true;
     }
 
-    else if (strncmp(buffer, "Args:", 5) == 0 && at_docs) {
-      before_cols = true;
+    else if (strncmp(buffer, "Args:", 5) == 0 && cur_line_at_docs) {
+      cur_line_before_cols = true;
     }
   }
 }
