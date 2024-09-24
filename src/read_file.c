8be6c9a197244d3c146747a18c2c24edd92977e7
Niklas Maderbacher <maderb190007@sr.htlweiz.at>
2024-09-24 21:59:11 +0200

    Save 09-24-2024 21-59-05

--- src/read_file.c
+++ src/read_file.c
@@ -6,49 +6,55 @@ void close_file(const FILE *file) { fclose(file); }
 
 struct DBSingleLinkedList extract_documentation_py(const FILE *py_file) {
   char buffer[256];
-  char name = NULL;
-  char cols_name = NULL;
-  bool in_docs = false;
+  bool at_docs = false;
   bool name_saved = false;
+  bool before_cols = false;
   bool at_cols = false;
-  bool end_of_docs = false;
-  DBSingleLinkedList entire_db;
-  DBTableSingleLinkedList db_table;
-  DBColumnSingleLinkedList db_column;
+  DBSingleLinkedList *entire_db;
+  DBTableSingleLinkedList *db_table;
+  DBColumnSingleLinkedList *db_column;
 
   // set last element pointers to NULLPOINTER
-  entire_db.next_struct = NULL;
-  db_table.next_struct = NULL;
-  db_column.next_struct = NULL;
+  entire_db->next_struct = NULL;
+  db_table->next_struct = NULL;
+  db_column->next_struct = NULL;
 
   while (fgets(buffer, sizeof(buffer), py_file) != NULL) {
-    // end of documentation, reset all values
-    if ((strncmp(buffer, "__tablename__", 13) == 0) && in_docs && at_cols) {
-      in_docs = false;
-      at_cols = false;
-      name_saved = false;
-    }
-
     char old_buffer[256];
     // remove leading spaces for easier work
     RemoveLeadingSpaces(buffer);
 
-    if (in_docs && strncmp(buffer, "\"\"\"", 3) != 0 && !at_cols &&
+    // end of documentation, reset all values
+    if ((strncmp(buffer, "__tablename__", 13) == 0) && at_docs && at_cols) {
+      at_docs = false;
+      at_cols = false;
+      name_saved = false;
+
+      // set final
+      entire_db->tables = db_table;
+    }
+
+    if (before_cols && at_docs) {
+      at_cols = true;
+      before_cols = false;
+    }
+
+    if (at_docs && strncmp(buffer, "\"\"\"", 3) != 0 && !at_cols &&
         !name_saved) {
-      db_table.table_name = StoreUntilSpace(buffer);
+      db_table->table_name = StoreUntilSpace(buffer);
       name_saved = true;
     }
 
-    else if (in_docs && at_cols) {
+    else if (at_docs && at_cols) {
     }
 
     // check if docs begin or end
-    if (strncmp(buffer, "class", 5) == 0 && !in_docs) {
-      in_docs = true;
+    if (strncmp(buffer, "class", 5) == 0 && !at_docs) {
+      at_docs = true;
     }
 
-    else if (strncmp(buffer, "Args:", 5) == 0 && in_docs) {
-      at_cols = true;
+    else if (strncmp(buffer, "Args:", 5) == 0 && at_docs) {
+      before_cols = true;
     }
   }
 }
