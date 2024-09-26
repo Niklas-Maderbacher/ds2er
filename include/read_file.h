10574c8cd2aa8c2ffafd321a0eb6f022e62fff75
Niklas Maderbacher <maderb190007@sr.htlweiz.at>
2024-09-26 21:13:11 +0200

    replaced {} with ; in header file

--- include/read_file.h
+++ include/read_file.h
@@ -22,16 +22,16 @@ const GetPreviousNodeFunc prev_table_node;
 /// @brief reads entire file and returns it as FILE pointer variable
 /// @param filepath (const char*) path to file
 /// @return (FILE*) returns entire file
-FILE *read_file(const char *filepath) {}
+FILE *read_file(const char *filepath);
 
 /// @brief closes file
 /// @param file
-void close_file(const FILE *file) {}
+void close_file(const FILE *file);
 
 /// @brief extract documentation from python file
 /// @param py_file (FILE*) python file
 /// @return SingleLinkedList including all tables with columns
 struct DBTableSingleLinkedListNode
-extract_documentation_py(const FILE *py_file) {}
+extract_documentation_py(const FILE *py_file);
 
 #endif
--- lib/include/remove_blank_space.h
+++ lib/include/remove_blank_space.h
@@ -1,12 +1,12 @@
 #ifndef REMOVE_BLANK_SPACE_H
 #define REMOVE_BLANK_SPACE_H
 
-void RemoveSpace(char *input) {}
+void RemoveSpace(char *input);
 
-void RemoveLeadingSpaces(char *input) {}
+void RemoveLeadingSpaces(char *input);
 
-char StoreUntilSpace(const char *input) {}
+char StoreUntilSpace(const char *input);
 
-char ExtractValueInBrackets(const char *input) {}
+char ExtractValueInBrackets(const char *input);
 
 #endif
